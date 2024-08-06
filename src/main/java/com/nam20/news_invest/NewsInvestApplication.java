b248139c7127a2eccdf2194e3c95632bc73128b7
nam20 <nam741585@naver.com>
2024-08-06 13:23:21 +0900

    init

---
+++ src/main/java/com/nam20/news_invest/NewsInvestApplication.java
@@ -0,0 +1,13 @@
+package com.nam20.news_invest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsInvestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsInvestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nam20/news_invest/NewsInvestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nam20.news_invest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsInvestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
