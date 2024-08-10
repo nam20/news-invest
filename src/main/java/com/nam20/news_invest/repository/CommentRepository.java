1ab9edf9d58be51b35cb040f511eb43efeeea886
nam20 <nam741585@naver.com>
2024-08-10 22:27:41 +0900

    feat: 댓글 Repository 메서드 추가

--- src/main/java/com/nam20/news_invest/repository/CommentRepository.java
+++ src/main/java/com/nam20/news_invest/repository/CommentRepository.java
@@ -3,5 +3,8 @@ package com.nam20.news_invest.repository;
 import com.nam20.news_invest.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findByUserId(Long userId);
 }
