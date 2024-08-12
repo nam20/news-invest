112d27391dfa49627643b9a77df15eb5859af848
nam20 <nam741585@naver.com>
2024-08-12 19:22:30 +0900

    feat: 유저 Repository 메서드 추가

--- src/main/java/com/nam20/news_invest/repository/UserRepository.java
+++ src/main/java/com/nam20/news_invest/repository/UserRepository.java
@@ -3,5 +3,10 @@ package com.nam20.news_invest.repository;
 import com.nam20.news_invest.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByName(String name);
+    Boolean existsByName(String name);
+    Boolean existsByEmail(String email);
 }
