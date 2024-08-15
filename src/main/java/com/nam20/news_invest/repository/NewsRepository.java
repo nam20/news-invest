da390cd23e314bd956fcd991db3bad9a6787cc26
nam20 <nam741585@naver.com>
2024-08-15 22:51:05 +0900

    remove: News API 제거

--- src/main/java/com/nam20/news_invest/repository/NewsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.nam20.news_invest.repository;
-
-import com.nam20.news_invest.entity.News;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NewsRepository extends JpaRepository<News, Long> {
-}
