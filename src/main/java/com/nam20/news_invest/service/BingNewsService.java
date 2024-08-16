08a2fbd80ecf9beb919618f71abf1bbc855c6485
nam20 <nam741585@naver.com>
2024-08-17 00:35:15 +0900

    feat: Bing News data DB 저장 로직 추가

--- src/main/java/com/nam20/news_invest/service/BingNewsService.java
+++ src/main/java/com/nam20/news_invest/service/BingNewsService.java
@@ -1,28 +1,41 @@
 package com.nam20.news_invest.service;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.nam20.news_invest.entity.NewsArticle;
+import com.nam20.news_invest.repository.NewsArticleRepository;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
+import reactor.core.publisher.Flux;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import java.util.List;
 
 @Service
 public class BingNewsService {
 
     private final WebClient webClient;
+    private final NewsArticleRepository newsArticleRepository;
 
     @Value("${BIND_NEWS_SEARCH_API_KEY}")
     private String apiKey;
 
     private List<String> searchQueries = List.of("stock");
 
-    public BingNewsService(WebClient.Builder webClientBuilder) {
+    public BingNewsService(WebClient.Builder webClientBuilder,
+                           NewsArticleRepository newsArticleRepository) {
         this.webClient = webClientBuilder.baseUrl("https://api.bing.microsoft.com").build();
+        this.newsArticleRepository = newsArticleRepository;
     }
 
-    public Mono<String> getNews(String searchQuery) {
-        return webClient.get()
+    public void processAllSearchQueries() {
+        searchQueries.forEach(this::getAndSaveNews);
+    }
+
+    private void getAndSaveNews(String searchQuery) {
+        webClient.get()
                 .uri(uriBuilder -> uriBuilder
                         .path("/v7.0/news/search")
                         .queryParam("q", searchQuery)
@@ -31,6 +44,23 @@ public class BingNewsService {
                         .build())
                 .header("Ocp-Apim-Subscription-Key", apiKey)
                 .retrieve()
-                .bodyToMono(String.class);
+                .bodyToMono(JsonNode.class)
+                .flatMapMany(jsonNode -> Flux.fromIterable(jsonNode.get("value")))
+                .map(this::convertToNewsArticle)
+                .subscribe(newsArticleRepository::save);
+    }
+
+    private NewsArticle convertToNewsArticle(JsonNode article) {
+        Instant instant = Instant.parse(article.get("datePublished").asText());
+        LocalDateTime publishedAt = LocalDateTime.ofInstant(instant, ZoneOffset.UTC);
+
+        return NewsArticle.builder()
+                .title(article.get("name").asText())
+                .url(article.get("url").asText())
+                .description(article.get("description").asText())
+                .thumbnailUrl(article.get("image").get("thumbnail").get("contentUrl").asText())
+                .provider(article.get("provider").get(0).get("name").asText())
+                .publishedAt(publishedAt)
+                .build();
     }
 }
