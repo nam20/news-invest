324526a4801aa370eda13560e5451881091487b7
nam20 <nam741585@naver.com>
2024-08-26 00:55:32 +0900

    refactor: Auth Controller 중복 로직 제거

--- src/main/java/com/nam20/news_invest/controller/AuthController.java
+++ src/main/java/com/nam20/news_invest/controller/AuthController.java
@@ -4,6 +4,8 @@ import com.nam20.news_invest.dto.AuthResponse;
 import com.nam20.news_invest.dto.LoginRequest;
 import com.nam20.news_invest.dto.RegisterRequest;
 import com.nam20.news_invest.dto.UserResponse;
+import com.nam20.news_invest.entity.User;
+import com.nam20.news_invest.mapper.UserMapper;
 import com.nam20.news_invest.security.JwtGenerator;
 import com.nam20.news_invest.service.UserService;
 import jakarta.validation.Valid;
@@ -26,43 +28,46 @@ public class AuthController {
     private final AuthenticationManager authenticationManager;
     private final JwtGenerator jwtGenerator;
     private final UserService userService;
+    private final UserMapper userMapper;
 
     @PostMapping("/register")
     public ResponseEntity<AuthResponse> register(@Valid @RequestBody RegisterRequest registerRequest) {
-
         UserResponse userResponse = userService.createUser(registerRequest);
-        String token = retrieveToken(registerRequest.getName(), registerRequest.getPassword());
-
-        AuthResponse authResponse = AuthResponse.builder()
-                .user(userResponse)
-                .token(token)
-                .build();
-
-        return ResponseEntity.ok(authResponse);
+        return authenticateAndGenerateResponse(
+                registerRequest.getName(), registerRequest.getPassword(), userResponse);
     }
 
     @PostMapping("/login")
     public ResponseEntity<AuthResponse> login(@Valid @RequestBody LoginRequest loginRequest) {
-
-        String token = retrieveToken(loginRequest.getName(), loginRequest.getPassword());
-        UserResponse userResponse = userService.retrieveUserByName(loginRequest.getName());
-
-        AuthResponse authResponse = AuthResponse.builder()
-                .user(userResponse)
-                .token(token)
-                .build();
-
-        return ResponseEntity.ok(authResponse);
+        return authenticateAndGenerateResponse(
+                loginRequest.getName(), loginRequest.getPassword(), null);
     }
 
-    private String retrieveToken(String username, String password) {
-
+    private ResponseEntity<AuthResponse> authenticateAndGenerateResponse(
+            String username, String password, UserResponse preCreatedUserResponse
+    ) {
         Authentication authentication = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(username, password)
         );
 
         SecurityContextHolder.getContext().setAuthentication(authentication);
 
-        return jwtGenerator.generateToken(authentication);
+        String token = jwtGenerator.generateToken(authentication);
+
+        UserResponse userResponse;
+
+        if (preCreatedUserResponse == null) {
+            User user = (User) authentication.getPrincipal();
+            userResponse = userMapper.toDto(user);
+        } else {
+            userResponse = preCreatedUserResponse;
+        }
+
+        AuthResponse authResponse = AuthResponse.builder()
+                .user(userResponse)
+                .token(token)
+                .build();
+
+        return ResponseEntity.ok(authResponse);
     }
 }
