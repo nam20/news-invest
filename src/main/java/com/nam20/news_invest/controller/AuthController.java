f59cb31f567de87601bcca37fe8ffa942f4dd949
nam20 <nam741585@naver.com>
2024-09-28 19:23:10 +0900

    feat: 로그인 여부 확인 API 추가

--- src/main/java/com/nam20/news_invest/controller/AuthController.java
+++ src/main/java/com/nam20/news_invest/controller/AuthController.java
@@ -15,11 +15,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -58,6 +56,15 @@ public class AuthController {
         return ResponseEntity.noContent()
                 .header(HttpHeaders.SET_COOKIE, cookie.toString())
                 .build();
+    }
+
+    @GetMapping("/check")
+    public ResponseEntity<UserResponse> refresh(@AuthenticationPrincipal User user) {
+        if (user != null) {
+            return ResponseEntity.ok(userMapper.toDto(user));
+        } else {
+            return ResponseEntity.noContent().build();
+        }
     }
 
     private ResponseEntity<UserResponse> authenticateAndGenerateResponse(
