46a76c4e89e0fab25af5bbdb622a4043e0ff46df
nam20 <nam741585@naver.com>
2025-05-27 15:58:44 +0900

    refactor: Lombok 적용으로 코드 간소화

--- src/main/java/com/nam20/news_invest/security/oauth2/CustomOAuth2User.java
+++ src/main/java/com/nam20/news_invest/security/oauth2/CustomOAuth2User.java
@@ -5,7 +5,11 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.Collection;
 import java.util.Map;
+import lombok.Getter;
+import lombok.AllArgsConstructor;
 
+@Getter
+@AllArgsConstructor
 public class CustomOAuth2User implements OAuth2User {
 
     private String registrationId; // e.g., "google"
@@ -15,16 +19,6 @@ public class CustomOAuth2User implements OAuth2User {
     private String pictureUrl;
     private Collection<? extends GrantedAuthority> authorities;
     private Map<String, Object> attributes;
-
-    public CustomOAuth2User(String registrationId, String id, String email, String name, String pictureUrl, Collection<? extends GrantedAuthority> authorities, Map<String, Object> attributes) {
-        this.registrationId = registrationId;
-        this.id = id;
-        this.email = email;
-        this.name = name;
-        this.pictureUrl = pictureUrl;
-        this.authorities = authorities;
-        this.attributes = attributes;
-    }
 
     @Override
     public Map<String, Object> getAttributes() {
@@ -39,21 +33,5 @@ public class CustomOAuth2User implements OAuth2User {
     @Override
     public String getName() {
         return this.name;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPictureUrl() {
-        return pictureUrl;
-    }
-
-    public String getRegistrationId() {
-        return registrationId;
     }
 } 
