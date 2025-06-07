3aa686f8fa5eb856a70268ffc551994585c746cf
nam20 <nam741585@naver.com>
2025-06-07 17:15:15 +0900

    feat: AOP 설정 클래스 추가

---
+++ src/main/java/com/nam20/news_invest/config/AopConfig.java
@@ -0,0 +1,9 @@
+package com.nam20.news_invest.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class AopConfig {
+} 
