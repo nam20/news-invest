0b105533da167893a4a5f016fea598699c0e52b5
nam20 <nam741585@naver.com>
2025-06-08 15:01:12 +0900

    feat: JPA 트랜잭션 매니저 빈 추가

--- src/main/java/com/nam20/news_invest/config/AopConfig.java
+++ src/main/java/com/nam20/news_invest/config/AopConfig.java
@@ -1,9 +1,19 @@
 package com.nam20.news_invest.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import jakarta.persistence.EntityManagerFactory;
 
 @Configuration
 @EnableAspectJAutoProxy
 public class AopConfig {
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        return new JpaTransactionManager(entityManagerFactory);
+    }
 } 
